92ce6a6a8fd581e16418e7f1bfcb24f7f8309969
Artemka11 <122256593+Artemka11@users.noreply.github.com>
2023-01-12 16:04:59 +0800

    Add files via upload

---
+++ тестирование/main1.py
@@ -0,0 +1,6 @@
+b = int(input('введите год '))
+a = b%100
+if a==0:
+    print("yes")
+else:
+    print("no")
---
+++ тестирование/main3.py
@@ -0,0 +1,10 @@
+pol=input("какого вы пола m или f")
+let=int(input("сколько вам лет"))
+
+if pol=="f":
+    if let>=10 and let<=15:
+        print("YES")
+    else:
+        print("NO")
+else:
+    print("NO")
