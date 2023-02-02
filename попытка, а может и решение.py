699222bdcf165953d0d78745401081dc283898e8
Artemka11 <122256593+Artemka11@users.noreply.github.com>
2023-02-02 14:44:07 +0800

    Add files via upload

---
+++ попытка, а может и решение.py
@@ -0,0 +1,9 @@
+x1 = int (input("Введите x1"))
+x2 = int (input("Введите x2"))
+y1 = int (input("Введите y1"))
+y2 = int (input("Введите y2"))
+if x1 and x2 and y1 and y2 <= 8:
+    print ("no")
+if x1 == x2 or y1 == y2:
+    print ("ji ess")
+else: print ("no")
