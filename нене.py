7ac7e6cb7172e4cec93f3dc998f3da27618b9413
Artemka11 <122256593+Artemka11@users.noreply.github.com>
2023-03-02 11:34:06 +0300

    Add files via upload

---
+++ нене.py
@@ -0,0 +1,15 @@
+n = int(input())
+popit = 0
+aboba = 0
+chetn = 0
+while n != 0:
+ n % 10 == 3
+ popit =+1
+ posl = n // 10 == 0
+ aboba =+1
+ n //= 10
+ if n // 2:
+    chetn =+1 
+print (popit)
+print (aboba)
+print (chetn)
