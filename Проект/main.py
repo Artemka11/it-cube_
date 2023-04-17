1b7eaf21ac3fb3829fd67ba9a8f0ed262b0c7505
Artemka11 <122256593+Artemka11@users.noreply.github.com>
2023-04-17 10:49:38 +0300

    Add files via upload

--- Проект/main.py
+++ Проект/main.py
@@ -1,25 +1,26 @@
-import numpy as np
-text = open('kargo.txt', encoding='utf8').read()
-corpus = text.split()
-def make_pairs(corpus):
+import numpy as np # подключаем библиотеку numpy
+text = open('kargo.txt', encoding='utf8').read() # в переменную отправляем все содержимое файла
+corpus = text.split() # разбиваем текст на отдельные слова
+def make_pairs(corpus): # делаем новую функуию, определятель пар слов
     
-    for i in range(len(corpus)-1):
-        yield (corpus[i], corpus[i+1])
-pairs = make_pairs(corpus)
-word_dict = {}
+    for i in range(len(corpus)-1):  # перебираем все слова в корпус, не включая последний
+        yield (corpus[i], corpus[i+1]) # генерируем пару и задаем её как результат функции
+pairs = make_pairs(corpus) # этим генератором получаем все пары слов
+word_dict = {} # словарь на старте пуст
 
-for word_1, word_2 in pairs:
-    if word_1 in word_dict.keys():
-        word_dict[word_1].append(word_2)
-    else:
-        word_dict[word_1] = [word_2]
-        first_word = np.random.choice(corpus)
+for word_1, word_2 in pairs: # перебираем все слова по два слова из готового списка парных слов
+    if word_1 in word_dict.keys(): # если типа первое слово есть в словаре
+         word_dict[word_1].append(word_2) #то добавляем второе слово как продолжение первого
+    else: #иначе
+        word_dict[word_1] = [word_2] # создаём запись в словаре и указываем второе слово как продолжение первого
+        first_word = np.random.choice(corpus) # случайно выбирается первое слово для старта
 
-while first_word.islower():
-    first_word = np.random.choice(corpus)
-    chain = [first_word]
-n_words = 100
+while first_word.islower(): # пока в нашем первом слове нет больших букв
+    first_word = np.random.choice(corpus) # то выбираем новое слово случайным образом
+                                          # и так до тех пор, пока не найдём слово с большой буквой
+    chain = [first_word] # делаем наше первое слово первым звеном
+n_words = int(input("количество слов для проверки вашей скорости печати ЫЫ")) # сколько слов будет в готовом тексте
 
-for i in range(n_words):
-    chain.append(np.random.choice(word_dict[chain[-1]]))
-print(' '.join(chain))
+for i in range(n_words): # делаем цикл с нашим количеством слов
+    chain.append(np.random.choice(word_dict[chain[-1]])) # на каждом шаге добавляем следующее слово из словаря, выбирая его случайным образом из доступных вариантов
+print(' '.join(chain)) #результат выводится! текст готов
